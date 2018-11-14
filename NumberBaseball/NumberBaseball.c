13102173fa6fda7dacca26738798822b4091c58c
Kyle Scheffler <fox1111784@hotmail.com>
2018-11-13 19:13:18 -0800

    -Moved over content from Update() to Render() -Edited content in Update()

--- NumberBaseball/NumberBaseball.c
+++ NumberBaseball/NumberBaseball.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define LINESIZE (1024)
 
-void Init(int *number1, int *number2, int *number3)
+void Init(int* number1, int* number2, int* number3)
 {
 	int test1;
 	int test2;
 	int test3;
+	time_t t;
+	srand((unsigned) &t);
+
 	while (1)
 	{
 		test1 = rand() % 10;
@@ -24,118 +28,111 @@ void Init(int *number1, int *number2, int *number3)
 		*number3 = test3;
 		break;
 	}
-	printf("Welcome to Number Baseball!\n");
 }
 
-void Update(int *number1, int *number2, int *number3) {
-	int guess1;
-	int guess2;
-	int guess3;
-	while (1) {
+void Update(int* number1, int* number2, int* number3, int* guess1, int* guess2, int* guess3, int* entry)
+{
+	while (*entry == 1)
+	{
 		char line[LINESIZE];
 		int numbersEntered = 0;
-		guess1 = -1;
-		guess2 = -1;
-		guess3 = -1;
-		printf("Choose 3 numbers:\n");
-		if (!fgets(line, LINESIZE, stdin)) {
+		*guess1 = -1;
+		*guess2 = -1;
+		*guess3 = -1;
+
+		if (!fgets(line, LINESIZE, stdin))
+		{
 			clearerr(stdin);
 			printf("\n");
-			continue;
+			exit(1);
 		}
-		if ((numbersEntered = sscanf(line, "%d %d %d",
-			&guess1, &guess2, &guess3)) == 3)
+
+		if ((numbersEntered = sscanf_s(line, "%d %d %d",
+			guess1, guess2, guess3)) == 3)
 		{
-			int correctCount = 0;
-
-			if (guess1 == *number1)
-			{
-				printf("Strike! ");
-				correctCount++;
-			}
-			else if (guess1 == *number2 || guess1 == *number3)
-			{
-				printf("Out! ");
-			}
-			else
-			{
-				printf("Ball! ");
-			}
-
-			if (guess2 == *number2)
-			{
-				printf("Strike! ");
-				correctCount++;
-			}
-			else if (guess2 == *number1 || guess2 == *number3)
-			{
-				printf("Out! ");
-			}
-			else
-			{
-				printf("Ball! ");
-			}
-
-			if (guess3 == *number3)
-			{
-				printf("Strike!\n");
-				correctCount++;
-			}
-			else if (guess3 == *number1 || guess3 == *number2)
-			{
-				printf("Out!\n");
-			}
-			else
-			{
-				printf("Ball!\n");
-			}
-
-			if (correctCount == 3) {
-				printf("You win!\n");
-				exit(1);
-			}
-			else
-			{
-				printf("Try again!\n");
-			}
-			clearerr(stdin);
+			return;
 		}
-		else if (numbersEntered > 3 || numbersEntered < 3)
+		else
 		{
-			printf("Invalid amount of numbers. Please choose 3 numbers.\n");
-			continue;
+			*entry = -1;
+			return;
 		}
 	}
 }
 
-/*
-void Render() {
+int Render(int* number1, int* number2, int* number3, int* guess1, int* guess2, int* guess3, int* entry)
+{
+	if (*entry == 0)
+	{
+		printf("Welcome to Number Baseball!\n");
+		printf("Choose 3 numbers:\n");
+		*entry = 1;
+	}
+	else if (*entry == -1)
+	{
+		printf("Invalid amount of numbers. Please choose 3 numbers.\n");
+		*entry = 1;
+	}
+	else
+	{
+		int correctCount = 0;
+		int* guess[] = {guess1, guess2, guess3};
+		int* number[] = {number1, number2, number3, number1, number2};
+		size_t i;
+
+		for (i = 0; i < 3; i++)
+		{
+			if (*guess[i] == *number[i])
+			{
+				printf("Strike! ");
+				correctCount++;
+			}
+			else if (*guess[i] == *number[0] || *guess[i] == *number[1] || *guess[i] == *number[2])
+			{
+				printf("Out! ");
+			}
+			else
+			{
+				printf("Ball! ");
+			}
+		}
+
+		if (correctCount == 3)
+		{
+			printf("You win!\n");
+			return 1;
+		}
+		else
+		{
+			printf("Try again!\n");
+		}
+	}
+	return 0;
+}
+
+void Release()
+{
 
 }
-*/
-
-/*
-void Release() {
-
-}
-*/
 
 int main(void)
 {
 	int random1;
 	int random2;
 	int random3;
-	int *n1 = &random1;
-	int *n2 = &random2;
-	int *n3 = &random3;
+	int guess1;
+	int guess2;
+	int guess3;
+	int endGame = 0;
+	int entry = 0;
 
-	Init(n1, n2, n3);
-	while (1)
+	Init(&random1, &random2, &random3);
+	while (endGame == 0)
 	{
-		Update(n1, n2, n3);
-		/* Render(); */
+		Update(&random1, &random2, &random3, &guess1, &guess2, &guess3, &entry);
+		endGame = Render(&random1, &random2, &random3, &guess1, &guess2, &guess3, &entry);
 	}
-	/* Release(); */
+	Release();
 
 	return 0;
 }
