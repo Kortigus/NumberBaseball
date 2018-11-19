8eebe9daf56013be1aa1984a4ee986c010683afc
Kyle Scheffler <fox1111784@hotmail.com>
2018-11-18 18:57:47 -0800

    Updated Init(), Update(), and Render(), so that they pass in structs instead of a huger list of pointers

--- NumberBaseball/NumberBaseball.c
+++ NumberBaseball/NumberBaseball.c
@@ -2,15 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define LINESIZE (1024)
+#define LINE_SIZE (1024)
 
-void Init(int* number1, int* number2, int* number3)
+typedef struct
 {
-	int test1;
-	int test2;
-	int test3;
-	time_t t;
-	srand((unsigned) &t);
+	size_t guess1;
+	size_t guess2;
+	size_t guess3;
+} guess;
+
+void Init(guess* player, guess* computer)
+{
+
+	size_t test1;
+	size_t test2;
+	size_t test3;
+	player->guess1 = -1;
+	player->guess2 = -1;
+	player->guess3 = -1;
+	srand((unsigned)time(NULL));
 
 	while (1)
 	{
@@ -23,81 +33,85 @@ void Init(int* number1, int* number2, int* number3)
 			continue;
 		}
 
-		*number1 = test1;
-		*number2 = test2;
-		*number3 = test3;
+		computer->guess1 = test1;
+		computer->guess2 = test2;
+		computer->guess3 = test3;
 		break;
 	}
 }
 
-void Update(int* number1, int* number2, int* number3, int* guess1, int* guess2, int* guess3, int* entry)
+void Update(guess* player, int* entry)
 {
 	while (*entry == 1)
 	{
-		char line[LINESIZE];
-		int numbersEntered = 0;
-		*guess1 = -1;
-		*guess2 = -1;
-		*guess3 = -1;
+		char line[LINE_SIZE];
+		size_t test1 = -1;
+		size_t test2 = -1;
+		size_t test3 = -1;
+		size_t* guess1 = &test1;
+		size_t* guess2 = &test2;
+		size_t* guess3 = &test3;
 
-		if (!fgets(line, LINESIZE, stdin))
+		if (!fgets(line, LINE_SIZE, stdin))
 		{
 			clearerr(stdin);
-			printf("\n");
-			exit(1);
+			printf("Error reading numbers please try again or restart program.\n");
+			continue;
 		}
 
-		if ((numbersEntered = sscanf_s(line, "%d %d %d",
-			guess1, guess2, guess3)) == 3)
+		if ((sscanf_s(line, "%u %u %u", guess1, guess2, guess3)) == 3)
 		{
+			player->guess1 = test1;
+			player->guess2 = test2;
+			player->guess3 = test3;
 			return;
 		}
-		else
-		{
-			*entry = -1;
-			return;
-		}
+
+		*entry = -1;
+		return;
 	}
 }
 
-int Render(int* number1, int* number2, int* number3, int* guess1, int* guess2, int* guess3, int* entry)
+int Render(guess* computer, guess* player, int* entry)
 {
 	if (*entry == 0)
 	{
 		printf("Welcome to Number Baseball!\n");
-		printf("Choose 3 numbers:\n");
+		printf("Please choose 3 unique numbers from 0 to 9\n");
 		*entry = 1;
 	}
 	else if (*entry == -1)
 	{
-		printf("Invalid amount of numbers. Please choose 3 numbers.\n");
+		printf("Invalid amount of numbers.\n Please choose 3 unique numbers from 0 to 9.\n");
 		*entry = 1;
 	}
 	else
 	{
-		int correctCount = 0;
-		int* guess[] = {guess1, guess2, guess3};
-		int* number[] = {number1, number2, number3, number1, number2};
+		size_t strikeCount = 0;
+		size_t outCount = 0;
+		size_t ballCount = 0;
+		size_t* guess[] = {player->guess1, player->guess2, player->guess3};
+		size_t* number[] = {computer->guess1, computer->guess2, computer->guess3, computer->guess1, computer->guess2};
 		size_t i;
 
 		for (i = 0; i < 3; i++)
 		{
-			if (*guess[i] == *number[i])
+			if (guess[i] == number[i])
 			{
-				printf("Strike! ");
-				correctCount++;
+				strikeCount++;
 			}
-			else if (*guess[i] == *number[0] || *guess[i] == *number[1] || *guess[i] == *number[2])
+			else if (guess[i] == number[0] || guess[i] == number[1] || guess[i] == number[2])
 			{
-				printf("Out! ");
+				outCount++;
 			}
 			else
 			{
-				printf("Ball! ");
+				ballCount++;
 			}
 		}
+		printf("%u %s, %u %s, %u %s\n", strikeCount, "Strike", outCount, "Ball", ballCount, "Out");
 
-		if (correctCount == 3)
+		if (strikeCount == 3)
 		{
 			printf("You win!\n");
 			return 1;
@@ -117,20 +131,16 @@ void Release()
 
 int main(void)
 {
-	int random1;
-	int random2;
-	int random3;
-	int guess1;
-	int guess2;
-	int guess3;
+	guess player;
+	guess computer;
 	int endGame = 0;
 	int entry = 0;
 
-	Init(&random1, &random2, &random3);
+	Init(&player, &computer);
 	while (endGame == 0)
 	{
-		Update(&random1, &random2, &random3, &guess1, &guess2, &guess3, &entry);
-		endGame = Render(&random1, &random2, &random3, &guess1, &guess2, &guess3, &entry);
+		Update(&player, &entry);
+		endGame = Render(&computer, &player, &entry);
 	}
 	Release();
 
